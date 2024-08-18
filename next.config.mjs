5f17e133be226b6ab6abb4559fca0ab6661a9926
kenguru10 <tinyken20@gmail.com>
2024-08-19 02:01:50 +0800

    update the workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	basePath: "/2024portfolio",
+	output: "export", // <=== enables static exports
+	reactStrictMode: true,
+};
 
 export default nextConfig;
