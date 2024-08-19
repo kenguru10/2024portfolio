37a9f12f45cf0f7d1f1c882b0bea9562b5473bc3
kenguru10 <tinyken20@gmail.com>
2024-08-19 15:24:50 +0800

    update the animation of navbar

--- src/app/(main)/(page)/layout.tsx
+++ src/app/(main)/(page)/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { motion } from "framer-motion";
+
 type Props = {
 	children: React.ReactNode;
 };
@@ -5,26 +8,32 @@ type Props = {
 export default function Layout({ children }: Props) {
 	return (
 		<div>
-			<div className="flex gap-3 py-5 px-10 items-center justify-between">
-				<div className="flex items-center">
-					<div className="">
-						<a href="/2024portfolio/home" className="text-xl font-[500]">
-							Kenguru
-						</a>
+			<motion.div
+				initial={{ y: 20, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				transition={{ ease: "easeInOut", duration: 0.75 }}
+			>
+				<div className="flex gap-3 py-5 px-10 items-center justify-between">
+					<div className="flex items-center">
+						<div className="">
+							<a href="/2024portfolio/home" className="text-xl font-[500]">
+								Kenguru
+							</a>
+						</div>
+						<div className="flex gap-3 items-center ml-10">
+							<a href="/2024portfolio/home" className="text-[1em] font-[400]">
+								Home
+							</a>
+							<a className="text-[1em] font-[400]">Services</a>
+							<a className="text-[1em] font-[400]">About Me</a>
+							<a className="text-[1em] font-[400]">Contact Me</a>
+						</div>
 					</div>
-					<div className="flex gap-3 items-center ml-10">
-						<a href="/2024portfolio/home" className="text-[1em] font-[400]">
-							Home
-						</a>
-						<a className="text-[1em] font-[400]">Services</a>
-						<a className="text-[1em] font-[400]">About Me</a>
-						<a className="text-[1em] font-[400]">Contact Me</a>
-					</div>
+					<button className="flex px-7 py-1 border rounded-full border-[#DACDBC]">
+						<div className="">Projects</div>
+					</button>
 				</div>
-				<button className="flex px-7 py-1 border rounded-full border-[#DACDBC]">
-					<div className="">Projects</div>
-				</button>
-			</div>
+			</motion.div>
 			{children}
 		</div>
 	);
